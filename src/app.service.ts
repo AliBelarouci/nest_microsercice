ecd29182008e61ef290cbef06d1287b33e4e9c20
Ali <ali@soufnet.com>
2024-04-18 18:58:13 +0100

    fix version test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import * as fs from 'fs';
 @Injectable()
 export class AppService {
+  private readonly version: string;
+    constructor() {
+    // Read package.json file synchronously
+    const packageJson = fs.readFileSync('package.json', 'utf8');
+    // Parse package.json content as JSON
+    const { version } = JSON.parse(packageJson);
+    // Assign the version to the class property
+    this.version = version;
+  }
   getHello(): string {
-    return 'Hello World! V 1.0.1';
+    return `Hello World! V ${this.version}`;
   }
 }
