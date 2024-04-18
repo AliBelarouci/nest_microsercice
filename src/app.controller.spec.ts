838fb28645f32c19ece5e6dc381f0ed7b4f360c4
Ali <ali@soufnet.com>
2024-04-18 19:04:59 +0100

    remove fs file from test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import * as fs from 'fs';
+
 describe('AppController', () => {
   let appController: AppController;
-  let version: string;
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
@@ -12,15 +12,11 @@ describe('AppController', () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
-        // Read package.json to get the version
-    const packageJson = fs.readFileSync('package.json', 'utf8');
-    version = JSON.parse(packageJson).version;
   });
 
   describe('root', () => {
-      it('should return "Hello World!" with version', () => {
-      // Use the version retrieved from package.json in the assertion
-      expect(appController.getHello()).toBe('Hello World! V ' + version);
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
+
 @Injectable()
 export class AppService {
-  private readonly version: string;
-    constructor() {
-    // Read package.json file synchronously
-    const packageJson = fs.readFileSync('package.json', 'utf8');
-    // Parse package.json content as JSON
-    const { version } = JSON.parse(packageJson);
-    // Assign the version to the class property
-    this.version = version;
-  }
   getHello(): string {
-    return `Hello World! V ${this.version}`;
+    return 'Hello World!';
   }
 }
