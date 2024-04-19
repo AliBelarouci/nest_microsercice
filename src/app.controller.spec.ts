94b0fa864beab203fd2d3e0a092c987e6dc7640b
Ali <ali@soufnet.com>
2024-04-19 11:21:42 +0100

    add host name

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import * as os from 'os';
 describe('AppController', () => {
   let appController: AppController;
-
+  const hostname = os.hostname();
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
-
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe(`Hello World! Hostname: ${hostname}`);
     });
   });
 });
